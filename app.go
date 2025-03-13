34cb60955b5ab7f677a036ba921a038a5cae20a9
Neevs1 <neevan.redkar@gmail.com>
2025-03-13 23:16:01 +0530

    Added libraries and a basic statement

--- app.go
+++ app.go
@@ -1,14 +1,23 @@
-package main(
+/*
+ * A simple to-do list app using Go programming language, Cobra library for CLI and SQLite for database
+ * The aim is to create a cross platform (windows,linux,mac) app
+ * Author : Neevs1 (github.com/Neevs1)
+ *
+ */
+
+package main
+
+import (
+	_ "database/sql"
 	"fmt"
-	"string"
-	"strconv"
-	"database/sql"
-	_ "github.com/go-sql-driver/mysql"
-	
+	_ "fmt"
+	_ "strconv"
 
-
+	_ "github.com/mattn/go-sqlite3"
+	_ "github.com/spf13/cobra"
 )
 
 func main() {
+	fmt.Println("Welcome to this app!")
 
 }
